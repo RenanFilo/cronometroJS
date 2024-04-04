4f7cef04fc014f859150773e7d2fb81a88f1ac48
RenanFilo <141690286+RenanFilo@users.noreply.github.com>
2024-04-03 21:11:13 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,6 +14,9 @@ let milliseconds = 0;
 let pause = false;
 
 starBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
 
 function startTimer() {
     interval = setInterval(() => {
@@ -38,11 +41,47 @@ function startTimer() {
         }
     },10);
 
-    function formatTime(time) {
-        return time < 10 ? `0${time}` : time;
-    }
+    starBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    resetBtn.style.display = "block";
+   
+}
 
-    function formatMilliseconds(time) {
-        return time < 100 ? `${time}`.padStart(3,"0") : time;
-    }
+function resumeTimer (){
+    pause = false;
+    pauseBtn.style.display = "block";
+    resumeBtn.style.display = "none";
+}
+
+function pauseTimer (){
+    pause = true;
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "block";
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+function formatMilliseconds(time) {
+    return time < 100 ? `${time}`.padStart(3,"0") : time;
+}
+
+function resetTimer(){
+    clearInterval(interval);
+
+    pause = false;
+
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEl.textContent = "00";
+    secondsEl.textContent = "00";
+    millisecondsEl.textContent = "000";
+
+    starBtn.style.display = "block";
+    resetBtn.style.display = "none";
+    resumeBtn.style.display = "none";
+    pauseBtn.style.display ="none";
 }
